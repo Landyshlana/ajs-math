dbca669232a82ef170dbbd42a9abd2d8d5f5eed0
Landyshlana <135726642+Landyshlana@users.noreply.github.com>
2024-04-15 05:36:50 +0300

    commit1

---
+++ src/js/Magician.js
@@ -0,0 +1,13 @@
+import Character from './character';
+
+export default class Magician extends Character {
+    constructor(name,type='Magician'){
+        super(name,type)
+        this.distance = 100;
+        this.attack = null;
+        this.stoned = false;
+        
+
+     
+    }
+}
